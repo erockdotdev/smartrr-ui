87af288c2ae2ccb66f0dc3da53c376a10542284b
Eric Sanchez <eric@smartrr.com>
2023-04-14 00:26:23 -0400

    add polaris provider

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import { AppProvider as PolarisProvider } from '@shopify/polaris'
 import { Button } from './smartrr-ui-components/actions/Button'
 import { Tag } from './smartrr-ui-components/actions/Tag'
 
 import { Banner } from './smartrr-ui-components/feedback-indicators/Banner'
 import { TextField } from './smartrr-ui-components/inputs/TextField'
 
+export const PolarisUtilities = { PolarisProvider }
+
 export { Button, Tag, TextField }
 
-export const feedbackIndicators = { Banner }
+export const FeedbackIndicators = { Banner }
