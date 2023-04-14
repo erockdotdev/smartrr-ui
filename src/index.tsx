48e6db96b277e139b00600fc2ede751e1e28792e
Eric Sanchez <eric@smartrr.com>
2023-04-14 11:50:53 -0400

    bump version to 0.2.2

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import { AppProvider } from '@shopify/polaris'
 import { PolarisStyleProvider, PolarisProvider } from './smartrr-ui-components'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
